89da24afce6b2db225accc95a72083fc5f96b427
Cheonghyun Lee <lee@cheonghyun.com>
2019-08-01 23:22:38 +0900

    main 작성

--- src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
+++ src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
@@ -1,8 +1,22 @@
 package com.comduck.chatbot.discord;
 
-public class DiscordBotMain {
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-    public static void main(String[] args) {
+public class DiscordBotMain extends ListenerAdapter {
+
+    public static void main(String[] args) throws Exception {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "NjA2NDc1NzE4MzA1Nzc1NjM2.XULmjw.vYwYU3M816BsjuW-mXxXauGVVx4";
+        builder.setToken(token);
+
+        builder.buildAsync();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("We received a message from" + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
     }
 }
