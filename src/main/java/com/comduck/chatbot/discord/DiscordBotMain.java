d63690ce5b6fb9769e1beba108dddb656597ea5a
Cheonghyun Lee <lee@cheonghyun.com>
2019-08-01 23:09:02 +0900

    maven 수정

--- src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
+++ src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
@@ -1,34 +1,8 @@
 package com.comduck.chatbot.discord;
 
-import net.dv8tion.jda.JDA;
-import net.dv8tion.jda.JDABuilder;
-import net.dv8tion.jda.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.hooks.EventListener;
+public class DiscordBotMain {
 
-import javax.security.auth.login.LoginException;
-
-public class DiscordBotMain implements EventListener {
-
-    public static void main(String[] args)
-            throws LoginException
-    {
-        JDA jda = new JDABuilder("token").build();
-        jda.addEventListener(new MessageListener());
-    }
-
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
-        if (event.isFromType(ChannelType.PRIVATE))
-        {
-            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-                    event.getMessage().getContentDisplay());
-        }
-        else
-        {
-            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-                    event.getTextChannel().getName(), event.getMember().getEffectiveName(),
-                    event.getMessage().getContentDisplay());
-        }
+    public static void main(String[] args) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
     }
 }
