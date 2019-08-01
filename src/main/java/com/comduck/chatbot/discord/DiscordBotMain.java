36717f6d8b7b98e9fda8d87bff5a27eb4cfbd451
Cheonghyun Lee <lee@cheonghyun.com>
2019-08-01 23:32:23 +0900

    작동하는 코드 커밋

--- src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
+++ src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
@@ -1,6 +1,7 @@
 package com.comduck.chatbot.discord;
 
 import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -8,15 +9,20 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class DiscordBotMain extends ListenerAdapter {
 
     public static void main(String[] args) throws Exception {
+        new DiscordBotMain().start();
+    }
+
+    private void start() throws Exception {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "NjA2NDc1NzE4MzA1Nzc1NjM2.XULmjw.vYwYU3M816BsjuW-mXxXauGVVx4";
         builder.setToken(token);
 
+        builder.addEventListener(this);
         builder.buildAsync();
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        System.out.println("We received a message from" + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
+        System.out.println("We received a message from " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
     }
 }
