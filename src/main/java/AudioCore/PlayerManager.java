f7def0106c7ff08ba733e525e82560a2c80279af
KIBA <kjh030529@gmail.com>
2019-08-03 04:07:36 +0900

    음악기능 1차 완성, 리액션 기능 미완성 String.Format을 이용한 텍스트 정리

---
+++ src/main/java/AudioCore/PlayerManager.java
@@ -0,0 +1,138 @@
+package AudioCore;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerManager {
+    private static PlayerManager INSTANCE;
+    private final AudioPlayerManager playerManager;
+    private final Map<Long, GuildMusicManager> musicManagers;
+
+    private PlayerManager()
+    {
+        this.musicManagers = new HashMap<Long, GuildMusicManager>();
+
+        this.playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        AudioSourceManagers.registerLocalSource(playerManager);
+    }
+
+    //
+    public synchronized GuildMusicManager getGuildMusicManager(Guild guild)
+    {
+        long guildId = guild.getIdLong();
+        GuildMusicManager musicManager = musicManagers.get(guildId);
+
+        if(musicManager == null)
+        {
+            musicManager = new GuildMusicManager(playerManager);
+            musicManagers.put(guildId, musicManager);
+        }
+
+        guild.getAudioManager().setSendingHandler(musicManager.getSendHandler());
+
+        return  musicManager;
+    }
+
+    //트랙 로드
+    public void loadAndPlay(final MessageReceivedEvent event, final String trackUrl)
+    {
+        if(!isURL(trackUrl) && !trackUrl.startsWith("ytsearch:"))
+        {
+            event.getChannel().sendMessage("> Youtube또는 SoundCloud의 링크를 넣어주세요.").queue();
+        }
+
+        final GuildMusicManager musicManager = getGuildMusicManager(event.getGuild());
+
+        //트랙 로드, 에러 관련 처리/이벤트
+        playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
+            public void trackLoaded(AudioTrack track) {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0x244aff));
+                eb.addField(track.getInfo().title, String.format("곡이 대기열에 추가되었습니다.\n``%s``", event.getAuthor().getName()), false);
+                event.getChannel().sendMessage(eb.build()).queue();
+
+                play(musicManager, track);
+            }
+
+            public void playlistLoaded(AudioPlaylist audioPlaylist) {
+                AudioTrack firstTrack = audioPlaylist.getSelectedTrack();
+
+                if(firstTrack == null){
+                    firstTrack = audioPlaylist.getTracks().get(0);
+                }
+
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0x244aff));
+                eb.addField(
+                        String.format("첫곡 : %s", firstTrack.getInfo().title),
+                        String.format(
+                                "리스트가 대기열에 추가되었습니다.\n%s\n``%s``",
+                                audioPlaylist.getName(),
+                                event.getAuthor().getName()
+                ), false);
+                event.getChannel().sendMessage(eb.build()).queue();
+
+                play(musicManager, firstTrack);
+            }
+
+            public void noMatches() {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0xff3e3e));
+                eb.addField(trackUrl, String.format("곡을 찾을수 없습니다.\n``%s``", event.getAuthor().getName()), false);
+                event.getChannel().sendMessage(eb.build()).queue();
+            }
+
+            public void loadFailed(FriendlyException e) {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0xff3e3e));
+                eb.addField(trackUrl, String.format("곡을 찾을수 없습니다.\n``%s``", event.getAuthor().getName()), false);
+                event.getChannel().sendMessage(eb.build()).queue();
+                event.getChannel().sendMessage("> "+e.toString()).queue();
+            }
+        });
+    }
+
+    private boolean isURL(String Url)
+    {
+        try
+        {
+            new URL(Url);
+            return true;
+
+        } catch (MalformedURLException e) {
+            return false;
+        }
+    }
+
+    //큐에 넘기기
+    private void play(GuildMusicManager musicManager, AudioTrack track)
+    {
+        musicManager.scheduler.queue(track);
+    }
+
+    //스크립트 전체 인스턴스로 반환
+    public static synchronized PlayerManager getInstance() {
+        if(INSTANCE == null)
+        {
+            INSTANCE = new PlayerManager();
+        }
+        return INSTANCE;
+    }
+}
--- src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
+++ src/main/java/com/comduck/chatbot/discord/DiscordBotMain.java
@@ -1,5 +1,15 @@
 package com.comduck.chatbot.discord;
 
+import AudioCore.GuildMusicManager;
+import AudioCore.PlayerManager;
+import AudioCore.TrackScheduler;
+import com.sedmelluq.discord.lavaplayer.player.*;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import com.sun.org.apache.xpath.internal.functions.FuncFalse;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
@@ -9,8 +19,12 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.managers.AudioManager;
+import net.dv8tion.jda.core.requests.restaction.MessageAction;
 
+import javax.swing.plaf.basic.BasicSliderUI;
 import java.awt.Color;
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
 
 public class DiscordBotMain extends ListenerAdapter {
 
@@ -31,7 +45,7 @@ public class DiscordBotMain extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         //로그 출력
         System.out.println(String.format("{'Guild_ID': '%s', 'Author': '%s', 'Context': '%s'}", event.getGuild().getName(), event.getAuthor().getName(), event.getMessage().getContentRaw() ));
-        //System.out.println("[Log] " + event.getGuild().getName() + event.getAuthor().getName() + " : " + event.getMessage().getContentDisplay());
+        //System.out.println("[Log] " + event.getGuild().getName() + event.getAuthor().getName().getName() + " : " + event.getMessage().getContentDisplay());
 
         //커맨드 모음에 데이터 인풋
         boolean commandRun =  CommandInterface(event);
@@ -59,7 +73,7 @@ public class DiscordBotMain extends ListenerAdapter {
     }
 
     //명렁어
-    private void BotCommands(MessageReceivedEvent event, String msg)
+    private void BotCommands(final MessageReceivedEvent event, String msg)
     {
 
         if( msg.startsWith("test") )
@@ -72,17 +86,146 @@ public class DiscordBotMain extends ListenerAdapter {
             Message sendMsg = event.getMessage();
             wait_reaction(sendMsg, "⏹" );
         }
-        else if( msg.startsWith("join") )
+        else if( msg.startsWith("play") )
         {
+            String url = msg.replaceFirst("play ", "");
+
             VoiceChannel Vch = event.getMember().getVoiceState().getChannel();
             AudioManager audiomng = event.getGuild().getAudioManager();
             audiomng.openAudioConnection(Vch);
+
+            PlayerManager manager = PlayerManager.getInstance();
+            manager.loadAndPlay(event, url);
+            manager.getGuildMusicManager(event.getGuild()).player.setVolume(10);
+        }
+        else if( msg.startsWith("join") )
+        {
+            VoiceChannel Vch = event.getMember().getVoiceState().getChannel();
+            event.getChannel().sendMessage(String.format(
+                    "> %s 입장 ``%s``",
+                    Vch.getName(),
+                    event.getAuthor().getName()
+            ));
+            AudioManager audiomng = event.getGuild().getAudioManager();
+            audiomng.openAudioConnection(Vch);
+
+
+        }
+        else if( msg.startsWith("leave") )
+        {
+
+            VoiceChannel Vch = event.getGuild().getSelfMember().getVoiceState().getChannel();
+            event.getChannel().sendMessage(String.format(
+                    "> %s 퇴장 ``%s``",
+                    Vch.getName(),
+                    event.getAuthor().getName()
+            ));
+            event.getGuild().getAudioManager().closeAudioConnection();
+
+
         }
         else if( msg.startsWith("stop") )
         {
-            VoiceChannel Vch = event.getGuild().getSelfMember().getVoiceState().getChannel();
-            event.getGuild().getAudioManager().closeAudioConnection();
+            PlayerManager manager = PlayerManager.getInstance();
+            GuildMusicManager musicManager = manager.getGuildMusicManager(event.getGuild());
+            AudioPlayer player = musicManager.player;
+            TrackScheduler scheduler = musicManager.scheduler;
+
+
+
+            scheduler.getQueue().clear();
+            player.stopTrack();
+            player.setPaused(false);
+            event.getChannel().sendMessage(String.format(
+                    "> 대기열 재생 중지 ``%s``",
+                    event.getAuthor().getName()
+            ));
         }
+        else if( msg.startsWith("skip") )
+        {
+            PlayerManager manager = PlayerManager.getInstance();
+            GuildMusicManager musicManager = manager.getGuildMusicManager(event.getGuild());
+            AudioPlayer player = musicManager.player;
+            TrackScheduler scheduler = musicManager.scheduler;
+
+            if(player.getPlayingTrack() == null)
+            {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0xff6624));
+                eb.addField("경고 Warning", String.format(
+                        "대기열이 비어 있습니다.\n``%s``",
+                        event.getAuthor().getName().toString()
+                ), false);
+                event.getChannel().sendMessage(eb.build()).queue();
+            }
+            else
+            {
+                event.getChannel().sendMessage(String.format(
+                        "> 곡 스킵 ``%s``",
+                        event.getAuthor().getName()
+                ));
+                scheduler.nextTrack();
+            }
+
+
+        }
+        else if( msg.startsWith("tracklist") )
+        {
+            PlayerManager manager = PlayerManager.getInstance();
+            GuildMusicManager musicManager = manager.getGuildMusicManager(event.getGuild());
+            AudioPlayer player = musicManager.player;
+            TrackScheduler scheduler = musicManager.scheduler;
+
+            if(player.getPlayingTrack() == null){
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0xff6624));
+                eb.addField("경고 Warning", String.format(
+                        "대기열이 비어 있습니다.\n``%s``",
+                        event.getAuthor().getName().toString()
+                ), false);
+                event.getChannel().sendMessage(eb.build()).queue();
+            }
+            else
+            {
+                AudioTrackInfo info = player.getPlayingTrack().getInfo();
+
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(new Color(0x244aff));
+                eb.setTitle(String.format(
+                        "재생중 - %s [%s/%s]",
+                        info.title,
+                        formatTime(player.getPlayingTrack().getPosition()),
+                        formatTime(player.getPlayingTrack().getDuration())
+                ));
+                Queue playelist = scheduler.getQueue();
+                String str = "";
+                for(int i=1; playelist.size() != 0; i++){
+                   AudioTrack t = (AudioTrack) playelist.poll();
+                   str += String.format("%d. %s\n", i, t.getInfo().title);
+                }
+                eb.addField("TrackList", str, false);
+                event.getChannel().sendMessage(eb.build()).queue();
+            }
+
+
+
+        }
+    }
+
+    private String formatTime(long time)
+    {
+        final long h = time / TimeUnit.HOURS.toMillis(1);
+        final long m = time / TimeUnit.MINUTES.toMillis(1);
+        final long s = time % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
+
+        if(h != 0){
+            return String.format("%2d:%2d:%2d", h, m, s);
+        }
+        else
+        {
+            return String.format("%2d:%2d", m, s);
+        }
+
     }
 
     private void wait_reaction(Message msg, String emote)
