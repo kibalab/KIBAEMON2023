7220ae74c94b5a08eeea1228b9a01e7699984ef4
7OAST <rimal935@gmail.com>
2024-05-14 22:16:10 +0900

    Create MizoreStickerRepliesProcessor When Mizore send specfic stickers, bot would replay with default emoji

--- src/main/java/com/comduck/chatbot/discord/action/processor/MizoreStickerRepliesProcessor.java
+++ src/main/java/com/comduck/chatbot/discord/action/processor/MizoreStickerRepliesProcessor.java
@@ -4,12 +4,15 @@ import com.comduck.chatbot.discord.action.Permission;
 import com.comduck.chatbot.discord.action.Processor;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.awt.*;
 
 @Permission(guildId = "814906163111526412", channelId = "", userId = "365857982652743681") // 배칠수에서 미조레한테만 반응하기
+// Bot Test
+//@Permission(guildId = "1169720331292905562", channelId = "", userId = "341178729927671814 ")
 public class MizoreStickerRepliesProcessor implements Processor {
     @Override
     public void OnProcess(GenericMessageEvent e, String msg) {
@@ -20,10 +23,23 @@ public class MizoreStickerRepliesProcessor implements Processor {
         else{
 
             if(event.getMessage().getStickers().stream().findFirst().get().getId().equals("1237783879054790669")){
-                e.getChannel().sendMessage(":kissing_cat::stuffed_flatbread:").queue();
+                //이렘 빵콘
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F63D")).queue();
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F35E")).queue();
             }
             else if(event.getMessage().getStickers().stream().findFirst().get().getId().equals("1237963291545043024")){
-                e.getChannel().sendMessage(":pleading_face::broken_heart::birthday:").queue();
+                //키이나 쓸쓸한 파티
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F97A")).queue();
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F494")).queue();
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F382")).queue();
+            }
+            else if(event.getMessage().getStickers().stream().findFirst().get().getId().equals("1237784110055952424")){
+                //이렘 울먹
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F63F")).queue();
+            }
+            else if(event.getMessage().getStickers().stream().findFirst().get().getId().equals("1237784110055952424")){
+                //이렘 (화들짝)
+                event.getMessage().addReaction(Emoji.fromUnicode("U+1F640")).queue();
             }
         }
 
